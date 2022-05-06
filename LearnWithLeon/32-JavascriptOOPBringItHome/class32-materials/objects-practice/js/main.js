22d95c10d13918063057edaf426c6e95160b7408
Jonah <jnh.thompson@gmail.com>
2022-05-06 14:14:53 -0400

    Add complete class 32 notes and materials

--- LearnWithLeon/32-JavascriptOOPBringItHome/class32-materials/objects-practice/js/main.js
+++ LearnWithLeon/32-JavascriptOOPBringItHome/class32-materials/objects-practice/js/main.js
@@ -1,51 +1,106 @@
-// Come up with with a parent class
-// Extend that parent class into two children
-// Use Encapsulation, Abstraction, Inheritance, and Polymorphism 
-class Contractor{
-    constructor(name,role){
-        this._name = name
-        this._role = role
-    }
-    get name(){
-        return this._name
-    }
-    get role(){
-        return this._role
-    }
-    sayHello(){
-        console.log(`Hello, I am on the ${this._role} team at #100Devs!`)
-    }
+class Contractor {
+  constructor(name, role){
+    this._name = name
+    this._role = role
+  }
+
+  get name(){
+    return this._name
+  }
+  
+  get role(){
+    return this._role
+  }
+
+  sayHello(){
+    console.log(`Hello world, I am ${this.name} on the ${this.role} team at #100Devs`)
+  }
 }
+
 class Front extends Contractor{
-    constructor(name,role,tech){
-        super(name,role)
-        this._tech = tech
-    }
-    get tech(){
-        return this._tech
-    }
-    sayHello(){
-        console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
-    }
+  constructor(name, role, tech){
+    super(name, role)
+    this._tech = tech
+  }
+
+  get tech(){
+    return this._tech
+  }
+
+  sayHello(){
+    console.log(`Hello world, I am ${this.name} on the ${this.role} team at #100Devs. I use ${this.tech}`)
+  }
 }
+
 class Back extends Contractor{
-    constructor(name,role,tech){
-        super(name,role)
-        this._tech = tech
-    }
-    get tech(){
-        return this._tech
-    }
-    sayHello(){
-        console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
-    }
-}
-let bob = new Contractor('Bob','Front-end')
-let simba = new Front('Simba','Front-end','React')
-let machi = new Back('The Machine','Back-end','Node')
+  constructor(name, role, tech){
+    super(name, role)
+    this._tech = tech
+  }
 
-let agency = [bob,simba,machi]
+  get tech(){
+    return this._tech
+  }
 
-for(person of agency){
-    person.sayHello()
+  sayHello() {
+    console.log(`Hello world, I am ${this.name} on the ${this.role} team at #100Devs. I use ${this.tech}`)
+  }
 }
+
+let agency = []
+agency.push(new Contractor('Bob', 'Front-end'))
+agency.push(new Front('John', 'Front-end', 'React'))
+agency.push(new Back('Bob', 'Back-end', 'Ruby on Rails'))
+
+agency.forEach(el => el.sayHello())
+// // Come up with with a parent class
+// // Extend that parent class into two children
+// // Use Encapsulation, Abstraction, Inheritance, and Polymorphism 
+// class Contractor{
+//     constructor(name,role){
+//         this._name = name
+//         this._role = role
+//     }
+//     get name(){
+//         return this._name
+//     }
+//     get role(){
+//         return this._role
+//     }
+//     sayHello(){
+//         console.log(`Hello, I am on the ${this._role} team at #100Devs!`)
+//     }
+// }
+// class Front extends Contractor{
+//     constructor(name,role,tech){
+//         super(name,role)
+//         this._tech = tech
+//     }
+//     get tech(){
+//         return this._tech
+//     }
+//     sayHello(){
+//         console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+//     }
+// }
+// class Back extends Contractor{
+//     constructor(name,role,tech){
+//         super(name,role)
+//         this._tech = tech
+//     }
+//     get tech(){
+//         return this._tech
+//     }
+//     sayHello(){
+//         console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+//     }
+// }
+// let bob = new Contractor('Bob','Front-end')
+// let simba = new Front('Simba','Front-end','React')
+// let machi = new Back('The Machine','Back-end','Node')
+
+// let agency = [bob,simba,machi]
+
+// for(person of agency){
+//     person.sayHello()
+// }
